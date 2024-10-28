e9e72e599331eecfcf5944eb49ceeda6c59a026e
kimhaechang <khc9812121@gmail.com>
2024-10-28 20:38:18 +0900

    Time: 20 ms (15.9%), Space: 48.7 MB (5.3%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,89 +1,46 @@
 class Solution {
     static int[][] ans;
-    static int[] dy = {-1,1,0,0,1,1,-1,-1};
-    static int[] dx = {0,0,-1,1,-1,1,-1,1};
+    static int[] dy = {-1,1,0,0};
+    static int[] dx = {0,0,-1,1};
     static int n;
     static int m;
+    static boolean[][] v;
+    static Queue<int[]> queue;
     public int[][] updateMatrix(int[][] mat) {
         // implementation
         n = mat.length;
         m = mat[0].length;
         ans = new int[n][m];
-        // 왼쪽에서 오른쪽
-        // 오른쪽에서 왼쪽, 위에서 아래, 아래에서 위
-        int[][] map1 = new int[n][m];
-        int[][] map2 = new int[n][m];
-        int[][] map3 = new int[n][m];
-        int[][] map4 = new int[n][m];
-
-
+        v = new boolean[n][m];
+        queue = new ArrayDeque<>();
         for(int i = 0;i<n;i++) {
-            int sum = 0;
             for(int j = 0;j<m;j++) {
-                if (mat[i][j] != 0 && sum + mat[i][j] > 0) {
-                    sum += mat[i][j];
-                    map1[i][j] = sum;
-                } else if (mat[i][j] == 0) {
-                    sum = 0;
-                }
-            }
-            sum = 0;
-            for(int j = m-1;j>-1;j--) {
-                if (mat[i][j] != 0 && sum + mat[i][j] > 0) {
-                    sum += mat[i][j];
-                    map2[i][j] = sum;
-                } else if (mat[i][j] == 0) {
-                    sum = 0;
+                if (mat[i][j] == 0) {
+                    v[i][j] = true;
+                    queue.add(new int[]{i, j, 0});
+                } else {
+                    ans[i][j] = 987654321;
                 }
             }
         }
-        for(int i = 0;i<m;i++) {
-            int sum = 0;
-            for(int j = 0;j<n;j++) {
-                if (mat[j][i] != 0 && sum + mat[j][i] > 0) {
-                    sum += mat[j][i];
-                    map3[j][i] = sum;
-                } else if (mat[j][i] == 0) {
-                    sum = 0;
-                }
-            }
-            sum = 0;
-            for(int j = n-1;j>-1;j--) {
-                if (mat[j][i] != 0 && sum + mat[j][i] > 0) {
-                    sum += mat[j][i];
-                    map4[j][i] = sum;
-                } else if (mat[j][i] == 0) {
-                    sum = 0;
-                }
-            }
-        }
-        System.out.println(Arrays.deepToString(map1));
-        System.out.println(Arrays.deepToString(map2));
-        System.out.println(Arrays.deepToString(map3));
-        System.out.println(Arrays.deepToString(map4));
-        for(int i = 0;i<n;i++) {
-            for(int j= 0;j<m;j++) {
-                if (mat[i][j] == 0) continue;
-                int min = 987654321;
-                for(int dir = 0;dir<4;dir++) {
-                    int ny = i + dy[dir];
-                    int nx = j + dx[dir];
-                    if (OOB(ny, nx)) continue;
-                    if (dir == 0) {
-                        min = Math.min(map3[ny][nx] + 1, min);
-                    } else if (dir == 1) {
-                        min = Math.min(map4[ny][nx] + 1, min);
-                    } else if (dir == 2) {
-                        min = Math.min(map1[ny][nx] + 1, min);
-                    } else {
-                        min = Math.min(map2[ny][nx] + 1, min);
-                    }
-                }
-                ans[i][j] = min;
-            }
-        }
-        
+        bfs(mat);
         return ans;
+    }
+    static void bfs(int[][] map) {
+        while(!queue.isEmpty()) {
+            int[] now = queue.poll();
+            for(int dir = 0;dir<4;dir++) {
+                int ny = now[0] + dy[dir];
+                int nx = now[1] + dx[dir];
+                if (OOB(ny, nx) || v[ny][nx] || map[ny][nx] == 0) continue;
+                if (ans[ny][nx] > now[2] + 1) {
+                    v[ny][nx] = true;
+                    ans[ny][nx] = now[2] + 1;
+                    queue.add(new int[]{ny, nx, now[2] + 1});
+                }
+                 
+            }
+        }
     }
     
     static boolean OOB(int y, int x) {
