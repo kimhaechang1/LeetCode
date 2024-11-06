d13431c44b7ac618c121ae251d4c5b8e731f4818
kimhaechang <khc9812121@gmail.com>
2024-11-06 15:02:12 +0900

    Time: 1336 ms (5.94%), Space: 54.1 MB (5.25%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,27 +1,21 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        // 순서는 중요하지 않음
         HashSet<List<Integer>> result = new HashSet<>();
         Arrays.sort(nums);
-        for(int i = 0;i<nums.length;i++) {
-            int s = i + 1;
-            int e = nums.length - 1;
-            while(s < e) {
-                int sum = nums[i] + nums[s] + nums[e];
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[s], nums[e]));
-                    s++;
-                    e--;
-                    
-                } else if (sum < 0) {
-                    s++;
-                } else {
-                    e--;
+        for(int i = 2;i<nums.length;i++) {
+            int gap = 0 - nums[i];
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for(int j = 0;j<i;j++) {
+                int gap2 = gap - nums[j];
+                if (map.containsKey(gap2)) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[j]); list.add(nums[i]); list.add(gap2);
+                    result.add(list);
                 }
+                map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
             }
         }
-        
         answer.addAll(result);
         return answer;
     }
