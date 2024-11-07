d20843b0c3f3e89d7d8d944b89e0cfb64d7f2a11
kimhaechang <khc9812121@gmail.com>
2024-11-07 13:36:00 +0900

    Time: 29 ms (10.51%), Space: 42.7 MB (56.02%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+    public Node cloneGraph(Node node) {
+        Node[] nodePool = new Node[101];
+        if (node!= null) {
+            for(int i = 1;i<nodePool.length;i++) {
+                nodePool[i] = new Node(i, new ArrayList<>());
+            }
+            boolean[] v = new boolean[101];
+            v[node.val] = true;
+            dfs(node, nodePool, v);
+        }
+        return nodePool[1];
+    }
+    static void dfs(Node node, Node[] pool, boolean[] v) {
+        
+        
+        for(Node next: node.neighbors) {
+            pool[node.val].neighbors.add(pool[next.val]);
+        }
+
+        for(Node next: node.neighbors) {
+            if (v[next.val]) continue;
+            v[next.val] = true;
+            dfs(next, pool, v);
+        }
+        
+    }
+}
