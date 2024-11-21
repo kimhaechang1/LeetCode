6784cae4d26ec43064b2ea873a20f6cea9bbb7e2
kimhaechang <khc9812121@gmail.com>
2024-11-21 12:35:57 +0900

    Update 0076-minimum-window-substring.java

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,7 +1,9 @@
 class Solution {
     public String minWindow(String s, String t) {
         // s, t, 연속적인 문자열들 중 t 문자열내에 문자들이 포함되어있는 부분 문자열중 가장 작은 부분문자열
-        
+        // 두포인터로 푸는데, 첫 풀이는 시간복잡도가 O(T * S) 였다.
+        // 이를 O(T) 수준으로 낮추는 핵심 로직은, 어짜피 필요한 문자는 정해져있고, 현재 윈도우의 길이가 T가 되는 순간이 아닌
+        // t 문자열의 요구사항을 갖춘 부분 문자열이 되는순간을 찾아야 한다.
         HashMap<Character, Integer> origin = new HashMap<>();
         for(char ch: t.toCharArray()) origin.put(ch, origin.getOrDefault(ch, 0) + 1);
         HashMap<Character, Integer> window = new HashMap<>();
