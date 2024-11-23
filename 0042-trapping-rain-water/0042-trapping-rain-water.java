54746e8683939234a9caa314a704c1f475850d3a
kimhaechang <khc9812121@gmail.com>
2024-11-23 12:24:53 +0900

    Time: 6 ms (7.35%), Space: 45.6 MB (94.85%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -13,23 +13,23 @@ class Solution {
         */
         Stack<Integer> stack = new Stack<>();
         for(int i = 0;i<height.length;i++) {
-            if (height[i] == 0) continue;
 
-            // 만약 스택이 비었다면? 일단 넣어야지
-            // 스택이 비어있지 않다면? 스택의 peek보다 큰지 작은지 계산해야함
-            // 스택이 비어있지 않은데, 스택의 peek보다 작다면 일단 넣어야함
-            // 스택이 비어있지 않은데, 스택의 peek보다 크다면 자신보다 큰 값이 나올때 까지 빼내면서 
-            int prev = 0;
-            while(!stack.isEmpty()) {
-                int hIdx = stack.peek();
-                ans += (i - hIdx - 1) * (Math.min(height[hIdx], height[i]) - (prev));
-                prev = height[hIdx];
-                if (height[hIdx] > height[i]) break;
-                stack.pop();
+            // 내림차순 유지를 하려다가 보니 더 큰값이 들어오려 한다.
+            // 그러면 현재 넣을려는 값 -> right, 중앙값 stack.peek() -> mid, 바로 왼쪽 비교대상 stack.pop().peek();
+            // 어짜피 스택 내부는 내림차순을 유지하려고 할것이므로, 
+            while(!stack.isEmpty() && height[stack.peek()] < height[i]) {
+                int mid = stack.pop();
+                if (stack.isEmpty()) break;
+                int left = stack.peek();
+                int hh = Math.min(height[left] - height[mid], height[i] - height[mid]);
+                ans += (i - left - 1) * (hh);
             }
             stack.push(i);
         }
         
         return ans;
+    }
+    public void sol2(int[] height) {
+
     }
 }
