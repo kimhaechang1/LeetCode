a709bbf79dd4525ce6d20b698fd05336ea7af4a8
kimhaechang <khc9812121@gmail.com>
2024-12-03 11:54:00 +0900

    Time: 1735 ms (5.01%), Space: 73.9 MB (5.17%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.java
@@ -12,24 +12,38 @@ class Solution {
 
         // 시작시각을 기준으로 정렬해보면, 특정 작업을 선택해서 얻을 수 있는 최대이익은 정해져 있기에 메모해둘 수 있다.
         // 그리고 작업선택 기준은 현재 선택된 작업의 끝나는 시각과 가장 가까운 작업을 선택할 수 있다.
+
+        // 정렬을 하지않았다면, 현재 작업을 선택하지 않고 다음작업으로 시작할 때의 선택이 무작위가 되어버려서 문제가 발생한다.
         
         timeMap = new TreeMap<>();
+        
+        int[][] info = new int[startTime.length][3];
         for(int i = 0;i < startTime.length;i++) {
-            ArrayList<Integer> idxList = timeMap.containsKey(startTime[i]) ? timeMap.get(startTime[i]) : new ArrayList<>();
+            info[i][0] = startTime[i];
+            info[i][1] = endTime[i];
+            info[i][2] = profit[i];
+        }
+
+        Arrays.sort(info, (a, b)-> {
+            return a[0] - b[0];
+        });
+
+        for(int i = 0;i < startTime.length;i++) {
+            ArrayList<Integer> idxList = timeMap.containsKey(info[i][0]) ? timeMap.get(info[i][0]) : new ArrayList<>();
             idxList.add(i);
-            timeMap.put(startTime[i], idxList);
+            timeMap.put(info[i][0], idxList);
         }
 
         dp = new int[startTime.length];
         Arrays.fill(dp, -1); // 이익의 음수는 없으므로
 
-        dfs(0, startTime, endTime, profit);
+        dfs(0, info);
         
         return dp[0];
     }
-    static int dfs(int depth, int[] start, int[] end, int[] profit) {
+    static int dfs(int depth, int[][] info) {
 
-        if (depth >= start.length) {
+        if (depth >= info.length) {
             // 만약 선택 할 idx가 더이상 선택이 불가하다면, 이득도 당연히 없다.
             return 0;
         }
@@ -40,19 +54,18 @@ class Solution {
         
         dp[depth] = 0;
         // 현재 작업을 선택하고 이어붙인 경우
-        int a = profit[depth];
-        Map.Entry<Integer, ArrayList<Integer>> entry = timeMap.ceilingEntry(end[depth]);
+        int a = info[depth][2];
+        Map.Entry<Integer, ArrayList<Integer>> entry = timeMap.ceilingEntry(info[depth][1]);
         if (entry != null) {
             ArrayList<Integer> idxList = entry.getValue();
             for(int idx: idxList) {
-                a = Math.max(a, profit[depth] + dfs(idx, start, end, profit));
+                a = Math.max(a, info[depth][2] + dfs(idx, info));
             }
         }
         // 현재 작업을 선택안한 경우
-        int b = dfs(depth + 1, start, end, profit);
-
+        int b = dfs(depth + 1, info);
+        
         dp[depth] = Math.max(a, b);
-        System.out.println(Arrays.toString(dp));
         return dp[depth];
     }
 }
