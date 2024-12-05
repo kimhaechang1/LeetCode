a87568beb06e91370f144dc8d5a3004952c80033
kimhaechang <khc9812121@gmail.com>
2024-12-05 13:38:46 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -0,0 +1,48 @@
+class Solution {
+    static List<Integer> answer;
+    public List<Integer> findAnagrams(String s, String p) {
+        answer = new ArrayList<>();
+        // 연속된 구간이니까, 투포인터로 해볼 수 있을 듯
+        // 길이도 정해져 있어서
+        int[] cnt = new int[26];
+        HashSet<Character> set = new HashSet<>();
+        for(char pp: p.toCharArray()) {
+            cnt[pp - 'a']++;
+            set.add(pp);
+        }
+        int[] window = new int[26];
+        int index = 0;
+        int cc = 0;
+        int maxCnt = set.size();
+        
+        for(; index < Math.min(p.length(), s.length()); index++) {
+            if (window[s.charAt(index) - 'a'] + 1 == cnt[s.charAt(index) - 'a'] ) {
+                cc++;
+            }
+            window[s.charAt(index) - 'a']++;
+        }
+        int start = 0;
+        int end = index - 1;
+        
+        // System.out.println(Arrays.toString(cnt)+" cc: "+cc);
+        while(end < s.length()) {
+            // System.out.println("start: "+start+" end: "+end+" window: "+Arrays.toString(window));
+            if (cc == maxCnt) {
+                answer.add(start);
+            }
+            if (window[s.charAt(start) - 'a'] - 1 < cnt[s.charAt(start) - 'a']) {
+                cc--;
+            }
+            window[s.charAt(start) - 'a']--;
+            start++;
+            end++;
+            if (end == s.length()) break;
+            if (window[s.charAt(end) - 'a'] + 1 == cnt[s.charAt(end) - 'a']) {
+                cc++;
+            }
+            window[s.charAt(end) - 'a']++;
+        }
+        
+        return answer;
+    }
+}
