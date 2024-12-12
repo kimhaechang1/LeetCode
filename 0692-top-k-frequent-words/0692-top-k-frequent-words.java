293c199c3637be1f5a679e0f219e4fde27a5e0e8
kimhaechang <khc9812121@gmail.com>
2024-12-12 16:37:31 +0900

    Time: 7 ms (66.74%), Space: 45.1 MB (5.75%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -5,6 +5,11 @@ class Solution {
         String w;
         int cnt;
 
+        public Data(String w, int cnt) {
+            this.w = w;
+            this.cnt = cnt;
+        }
+
         public int compareTo(Data o) {
             if (this.cnt == o.cnt) {
                 return this.w.compareTo(o.w);
@@ -15,32 +20,16 @@ class Solution {
     public List<String> topKFrequent(String[] words, int k) {
         n = words.length;
         answer = new ArrayList<>();
-        Arrays.sort(words);
-        String prev = "";
-        int cnt = 0;
-        PriorityQueue<Data> pq = new PriorityQueue<>();
-        for(int i = 0;i < n ;i++) {
-            if (prev.equals(words[i])) {
-                cnt++;
-            } else {
-                Data newData = new Data();
-                newData.w = prev;
-                newData.cnt = cnt;
-                cnt = 1;
-                prev = words[i];
-                if (cnt == 0) continue;
-                pq.add(newData);
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String word: words) map.put(word, map.getOrDefault(word, 0) + 1);
+        TreeSet<Data> treeSet = new TreeSet<>();
+        for(String word: map.keySet()) {
+            treeSet.add(new Data(word, map.get(word)));
+            if (treeSet.size() > k) {
+                treeSet.pollLast();
             }
         }
-        Data newData = new Data();
-        newData.w = words[n - 1];
-        newData.cnt = cnt;
-        pq.add(newData);
-        
-        while(k-- > 0) {
-            answer.add(pq.poll().w);
-        }
-
+        while(k-- > 0) answer.add(treeSet.pollFirst().w);
         return answer;
     }
 }
