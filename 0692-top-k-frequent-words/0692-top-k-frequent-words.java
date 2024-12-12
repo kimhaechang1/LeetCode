2d1ad3000eebb0ef5544bd98c427fac3b694f0ff
kimhaechang <khc9812121@gmail.com>
2024-12-12 16:37:39 +0900

    Time: 7 ms (66.74%), Space: 44.6 MB (62.82%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,35 +1,21 @@
 class Solution {
     static int n;
     static List<String> answer;
-    static class Data implements Comparable<Data>{
-        String w;
-        int cnt;
-
-        public Data(String w, int cnt) {
-            this.w = w;
-            this.cnt = cnt;
-        }
-
-        public int compareTo(Data o) {
-            if (this.cnt == o.cnt) {
-                return this.w.compareTo(o.w);
-            }
-            return o.cnt - this.cnt;
-        }
-    }
     public List<String> topKFrequent(String[] words, int k) {
         n = words.length;
         answer = new ArrayList<>();
         HashMap<String, Integer> map = new HashMap<>();
         for(String word: words) map.put(word, map.getOrDefault(word, 0) + 1);
-        TreeSet<Data> treeSet = new TreeSet<>();
-        for(String word: map.keySet()) {
-            treeSet.add(new Data(word, map.get(word)));
-            if (treeSet.size() > k) {
-                treeSet.pollLast();
+        TreeSet<Map.Entry<String, Integer>> treeSet = new TreeSet<>((a, b) -> {
+            if (a.getValue() == b.getValue()) {
+                return a.getKey().compareTo(b.getKey());
             }
+            return b.getValue() - a.getValue();
+        });
+        for(Map.Entry<String, Integer> entry: map.entrySet()) {
+            treeSet.add(entry);
         }
-        while(k-- > 0) answer.add(treeSet.pollFirst().w);
+        while(k-- > 0) answer.add(treeSet.pollFirst().getKey());
         return answer;
     }
 }
