51d14d731271fb636ac730dc67ad417439063259
kimhaechang <khc9812121@gmail.com>
2024-12-12 16:40:15 +0900

    Time: 6 ms (97.85%), Space: 44.9 MB (17.79%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,21 +1,23 @@
 class Solution {
-    static int n;
-    static List<String> answer;
+
     public List<String> topKFrequent(String[] words, int k) {
-        n = words.length;
-        answer = new ArrayList<>();
         HashMap<String, Integer> map = new HashMap<>();
-        for(String word: words) map.put(word, map.getOrDefault(word, 0) + 1);
-        TreeSet<Map.Entry<String, Integer>> treeSet = new TreeSet<>((a, b) -> {
+        TreeSet<Map.Entry<String, Integer>> pq = new TreeSet<>((a, b) -> {
             if (a.getValue() == b.getValue()) {
                 return a.getKey().compareTo(b.getKey());
             }
             return b.getValue() - a.getValue();
         });
+        List<String> answer = new ArrayList<>();
+        
+        for(String word: words) map.put(word, map.getOrDefault(word, 0) + 1);
+        
         for(Map.Entry<String, Integer> entry: map.entrySet()) {
-            treeSet.add(entry);
+            pq.add(entry);
         }
-        while(k-- > 0) answer.add(treeSet.pollFirst().getKey());
+
+        while(k-- > 0) answer.add(pq.pollFirst().getKey());
+
         return answer;
     }
 }
