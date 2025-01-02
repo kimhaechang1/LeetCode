79302bcf4d26d103e4fb7dd7d41ae45076a5d31e
kimhaechang <khc9812121@gmail.com>
2025-01-02 14:16:29 +0900

    Time: 2 ms (59.03%), Space: 46.4 MB (18.23%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -3,6 +3,8 @@ class Solution {
         return solution2(nums1, nums2);
     }
     public double solution2(int[] nums1, int[] nums2) { 
+
+        // O(log min(n, m))
         if (nums1.length < nums2.length) {
             return sol(nums1, nums2);
         } else { 
@@ -10,21 +12,20 @@ class Solution {
         }
     }
     public double sol(int[] small, int[] large) {
-        // 전체 배열의 크기는 n + m이다.
-        // 여기서 중앙은 n + m / 2 일 것이다.
-
-        // 전체 중앙의 인덱스는 (n + m) / 2 이다.
-        // 여기서 중앙 인덱스를 기준으로 왼쪽의 원소들도 num1, nums2로 채워야 하고 반대 쪽도 마찬가지
-        // 왼쪽만 보고 생각하면 된다.
-        // out of bound가 안날려면 더 작은 배열을 기준으로 길이를 기준으로 잡아야 하고
-        // 작은 배열의 기준점을 mid1이라고 했을 때, 큰 배열은 (n + m) / 2 - mid1 만큼을 꺼내야 한다.
-        // 여기서 정답을 어떻게 판단하는가? 
-        // mid1 - 1, mid1 + 1, mid2 - 1, mid2 + 1 각 배열 경계선에 인접한 값이다.
-        // mid1 - 1의 경우 mid2 + 1보다도 작아야 한다, 반대로 mid1 + 1은 mid2 - 1 보다 커야 한다.
-        // 자명한 결정조건이 있으므로 이진탐색이 가능하다.
         
-        // 전체 중앙값이 (n + m) / 2 가 홀수인 경우와 짝수인 경우로 나눠서 생각해보면
-        // 홀수인 경우 
+        // 정렬되어 있는 배열 및 경계선을 핵심 키워드로 생각해보자.
+        // 전체 배열의 크기는 n + m 이다.
+        // 그러면 전체 배열에서 중앙까지 필요한 원소의 개수가 있을것, 이를 totalMid라고 하자.
+        // 그 중앙 개수를 채우는것은 반드시 small과 large의 특정 범위일 것이다.
+        // 아웃오브 바운드를 생각해서 small을 기준으로 범위를 지정하자.
+        // 그리고 small에서 merge 배열에 중앙까지 채울 개수를 lCnt, 반대를 rCnt라고 했을 때,
+        // lCnt - 1이 경계선에 놓인값이고 lCnt는 경계선 밖의 값
+        // rCnt도 마찬가지이다.
+        // 각 배열별 경계선에서 경계선보다 높은 값보다 서로의 반대편 경계선값은 작아야 한다.
+        // 무슨말이냐면 lCnt - 1이 l배열의 경계선, lCnt가 경계선 바로 밖의 값이라고 정의하고 r배열도 마찬가지로 정의하자.
+        // lCnt - 1 <= rCnt 면서 lCnt >= rCnt - 1이어야 한단 뜻이다. 이러면 정답을 구할 수 있다.
+        // 반대로 만약 lCnt - 1 > rCnt 가 되어버리면? 그 땐 l배열의 개수를 하나 줄이고 r배열의 개수를 하나 늘려야 한다. 
+        // 전체 개수는 항상 정해져 있음을 까먹지 말자
         int n = small.length;
         int m = large.length;
         int totalLeftCnt = (n + m) / 2 + (((n + m) & 1) == 1 ? 1 : 0);
@@ -72,6 +73,7 @@ class Solution {
     }
 
     public double solution1(int[] nums1, int[] nums2) {
+        // O(n + m)
         int[] arr = new int[nums1.length + nums2.length];
         int l = 0;
         int r = 0;
