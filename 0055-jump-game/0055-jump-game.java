f313f020e8c6266e14861c3bfb1aff84f7866177
kimhaechang <khc9812121@gmail.com>
2025-01-04 12:51:22 +0900

    Time: 2 ms (79.67%), Space: 46.3 MB (6.6%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,6 +1,4 @@
 class Solution {
-    static int[] memo;
-    static int n;
     public boolean canJump(int[] nums) {
         // 정수배열 num가 주어진다.
         // 너는 배열의 첫번째 인덱스에 위치해있다.
@@ -8,47 +6,16 @@ class Solution {
 
         // 마지막 인덱스에 도달이 가능하다면 true를, 불가능하다면 false를 반환하라
 
-        memo = new int[nums.length];
-        n = nums.length;
-        // 0: 가능, 1: 불가능, -1: 도달한 적 없음
-        Arrays.fill(memo, -1);
-        if (nums.length == 1) return true;
+        // 좀 쉽게 생각하면, 특정 범위까지 도달이 가능하다고 생각해보자.
+        // 그러면 도움닫기를 최대한 했을 때, 이미 nums.length - 1 이상이 된다면 그건 도달이 가능한것
 
-        int[] dp = new int[nums.length];
-        boolean[] isReached = new boolean[nums.length];
-        isReached[0] = true;
-        for(int i = 1; i < nums.length; i++) {
-            boolean flag = false;
-            for(int j = i - 1; j > -1; j--) {
-                if (i - j <= nums[j]) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) return false;
+        int max = 0; // 지금까지 최대한 갈 수 있는 길이
+        for(int i = 0; i < nums.length; i++) {
+            if (max < i) return false; // 도움닫기를 최대한 해봣는데도 i번째에 닿지 못하는경우가 있을 수 있다.
+            max = Math.max(max, i + nums[i]);
+            if (max >= nums.length - 1) return true;
         }
         
-        return true;
-    }
-    static int dfs(int depth, int[] nums) {
-        if (memo[0] == 0) return 0;
-        if (depth == n - 1) {
-            return 0;
-        }
-        if (memo[depth] != -1) {
-            return memo[depth];
-        }
-
-        memo[depth] = 1;
-        boolean isFind = false;
-        for(int step = 1; step <= nums[depth]; step++) {
-            if (depth + step < n && memo[depth + step] != 1) {
-                isFind = true;
-                memo[depth] = dfs(depth + step, nums);
-            }
-        }
-        if (!isFind) memo[depth] = 1;
-
-        return memo[depth];
+        return false;
     }
 }
