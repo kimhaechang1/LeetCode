12f09f61a7e39e22a7ee7193eb981e4bd61b609e
kimhaechang <khc9812121@gmail.com>
2025-01-05 12:51:38 +0900

    Time: 1 ms (100%), Space: 44.8 MB (25.7%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,52 +10,59 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode();
-        ListNode head = dummy;
-        System.out.println(dummy);
-        int carry = 0;
-        while(l1 != null && l2 != null){
-            int a1 = l1.val;
-            int a2 = l2.val;
-            int sum = a1 + a2 + carry;
-            boolean isCarry = false;
-            if(sum >= 10){
-                carry = 1;
-            }else{
-                carry = 0;
-            }
-            head.next = new ListNode(sum % 10);
-            head = head.next;
-            l1 = l1.next;
-            l2 = l2.next;
+        // 비어있지 않은 2 개의 연결리스가 주어진다. 각 연결리스트는 양의 정수를 표현하고 있다.
+        // 숫자정보는 역순으로 저장되어 있으며, 각 노드별로 하나의 1의 자리수만 포함하고 있다.
+        // 9 9 9 9 9 9 9
+        // +
+        //       9 9 9 9
+        // 1 0 0 0 9 9 9 8   
+        // 더 긴 링크드 리스트를 기준으로 작은 링크드 리스트의 노드의 값을 누적하고
+        // 캐리값이 있으면 앞에 1을 더하되, 현재가 가장 첫번째 노드라면 누적할 수 없다.
+        boolean isPrevCarry = false;
+        ListNode ptr1 = l1; ListNode ptr2 = l2;
+        
+        while(true) {
+            int val = ptr1.val + ptr2.val;
+            if (isPrevCarry) val++;
+            if (val >= 10) isPrevCarry = true;
+            else isPrevCarry = false;
+            val %= 10;
+            ptr1.val = val;
+            ptr2.val = val;
+            if (ptr1.next == null || ptr2.next == null) break;
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
         }
-        while(l1 != null){
-            int sum = l1.val + carry;
-            if(sum >= 10){
-                carry = 1;
-            }else{
-                carry = 0;
+        // 둘중 널이 아닌 리스트 포인터에 대해서 마지막 캐리 연산을 한다.
+        ListNode answer = new ListNode();
+        if (ptr1.next != null) {
+            while(isPrevCarry && ptr1.next != null) {
+                ptr1 = ptr1.next;
+                ptr1.val++;
+                if (ptr1.val >= 10) isPrevCarry = true;
+                else isPrevCarry = false;
+                ptr1.val %= 10;
             }
-            head.next = new ListNode(sum % 10);
-            head = head.next;
-            l1 = l1.next;
-        }
-        while(l2 != null){
-            int sum = l2.val + carry;
-            if(sum >= 10){
-                carry = 1;
-            }else{
-                carry = 0;
+            if (isPrevCarry && ptr1.next == null) {
+                ptr1.next = new ListNode(1);
             }
-            head.next = new ListNode(sum % 10);
-            head = head.next;
-            l2 = l2.next;
-        }
-        if(carry != 0){
-            head.next = new ListNode(carry);
+        } else if (ptr2.next != null) {
+            while(isPrevCarry && ptr2.next != null) {
+                ptr2 = ptr2.next;
+                ptr2.val++;
+                if (ptr2.val >= 10) isPrevCarry = true;
+                else isPrevCarry = false;
+                ptr2.val %= 10;
+            }
+            if (isPrevCarry && ptr2.next == null) {
+                ptr2.next = new ListNode(1);
+            }
+            return l2;
+        } else if (isPrevCarry && ptr1.next == null && ptr2.next == null){
+            ptr1.next = new ListNode(1);
         }
         
-        ListNode result = dummy.next;
-        return result;
+        
+        return l1;
     }
 }
